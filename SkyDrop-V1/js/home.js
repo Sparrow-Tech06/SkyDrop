a5a4e46a05e48481eaf2991d824d86d3211dbcb7
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-10 13:43:12 +0530

    Update home.js

--- SkyDrop-V1/js/home.js
+++ SkyDrop-V1/js/home.js
@@ -8,9 +8,13 @@ data.quizzes.forEach(quiz => {
 
 const card = `
 <div class="col-md-4 mb-3">
-<div class="card quiz-card p-3" onclick="openQuiz('${quiz.id}')">
+<div class="card p-3 shadow-sm" style="cursor:pointer"
+onclick="openQuiz('${quiz.id}')">
+
 <h5>${quiz.title}</h5>
+
 <p>${quiz.questions.length} Questions</p>
+
 </div>
 </div>
 `;
@@ -22,5 +26,7 @@ container.innerHTML += card;
 });
 
 function openQuiz(id){
+
 location.href = "quiz.html?quiz=" + id;
+
 }
