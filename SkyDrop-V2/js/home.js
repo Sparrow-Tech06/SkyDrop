0c7740af254373d7cd3d412874cd66ff1840a0e5
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-10 12:20:59 +0530

    Create home.js

---
+++ SkyDrop-V2/js/home.js
@@ -0,0 +1,26 @@
+fetch("data/quiz-data.json")
+.then(res => res.json())
+.then(data => {
+
+const container = document.getElementById("quizContainer");
+
+data.quizzes.forEach(quiz => {
+
+const card = `
+<div class="col-md-4 mb-3">
+<div class="card quiz-card p-3" onclick="openQuiz('${quiz.id}')">
+<h5>${quiz.title}</h5>
+<p>${quiz.questions.length} Questions</p>
+</div>
+</div>
+`;
+
+container.innerHTML += card;
+
+});
+
+});
+
+function openQuiz(id){
+location.href = "quiz.html?quiz=" + id;
+}
