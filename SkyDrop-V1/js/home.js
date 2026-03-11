09dda6dd709b53e1ba5962231f94b0327e6b43fa
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-11 09:42:48 +0530

    Update home.js

--- SkyDrop-V1/js/home.js
+++ SkyDrop-V1/js/home.js
@@ -1,21 +1,20 @@
 fetch("data/quiz-data.json")
-.then(res => res.json())
-.then(data => {
+.then(res=>res.json())
+.then(data=>{
 
 const container=document.getElementById("quizContainer");
 
-data.quizzes.forEach(quiz=>{
+data.quizzes.forEach(q=>{
 
 const card=`
 <div class="col-md-4 mb-3">
 
 <div class="card p-3 shadow-sm"
 style="cursor:pointer"
-onclick="openQuiz('${quiz.id}')">
+onclick="openQuiz('${q.id}')">
 
-<h5>${quiz.title}</h5>
-
-<p>${quiz.questions.length} Questions</p>
+<h5>${q.title}</h5>
+<p>${q.questions.length} Questions</p>
 
 </div>
 
