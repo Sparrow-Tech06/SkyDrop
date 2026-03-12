912b98f0b6572989bf9bf6d4f047aaf3d9f473dc
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-12 07:27:32 +0530

    Update wallet.js

--- SkyDrop-V1/js/wallet.js
+++ SkyDrop-V1/js/wallet.js
@@ -1,54 +1,98 @@
 let db;
 
-const request=indexedDB.open("QuizWallet",2);
+const request = indexedDB.open("QuizWallet",2);
 
-request.onsuccess=function(e){
+request.onupgradeneeded = function(e){
 
-db=e.target.result;
+db = e.target.result;
+
+if(!db.objectStoreNames.contains("wallet")){
+db.createObjectStore("wallet",{keyPath:"id"});
+}
+
+if(!db.objectStoreNames.contains("history")){
+db.createObjectStore("history",{autoIncrement:true});
+}
+
+};
+
+request.onsuccess = function(e){
+
+db = e.target.result;
 
 loadWallet();
 
 };
 
-function loadWallet(){
+request.onerror = function(){
 
-const tx=db.transaction(["wallet","history"],"readonly");
-
-const wallet=tx.objectStore("wallet");
-const history=tx.objectStore("history");
-
-wallet.get("user").onsuccess=function(e){
-
-let coins=0;
-
-if(e.target.result){
-coins=e.target.result.coins;
-}
-
-document.getElementById("coinAmount").innerText=
-coins+" Coins";
+console.log("DB Error");
 
 };
 
-const list=document.getElementById("historyList");
 
-history.openCursor().onsuccess=function(e){
+function loadWallet(){
 
-const cursor=e.target.result;
+const tx = db.transaction(["wallet","history"],"readonly");
+
+const walletStore = tx.objectStore("wallet");
+const historyStore = tx.objectStore("history");
+
+
+/* load total coins */
+
+walletStore.get("user").onsuccess = function(e){
+
+let coins = 0;
+
+if(e.target.result){
+coins = e.target.result.coins;
+}
+
+document.getElementById("coinAmount").innerText =
+coins + " Coins";
+
+};
+
+
+/* load history */
+
+const list = document.getElementById("historyList");
+
+list.innerHTML = "";
+
+historyStore.openCursor(null,"prev").onsuccess = function(e){
+
+const cursor = e.target.result;
 
 if(cursor){
 
-const item=cursor.value;
+const item = cursor.value;
 
-list.innerHTML+=`
-<li class="list-group-item">
-${item.game} +${item.coins}
+const li = document.createElement("li");
+
+li.className = "list-group-item";
+
+li.innerHTML = `
+<strong>${item.game}</strong>
+<span class="text-success"> +${item.coins}</span>
 <br>
-<small>${item.time}</small>
-</li>
+<small class="text-muted">${item.time}</small>
 `;
 
+list.appendChild(li);
+
 cursor.continue();
+
+}
+
+else{
+
+if(list.innerHTML === ""){
+list.innerHTML = `<li class="list-group-item text-muted">
+No coin history yet
+</li>`;
+}
 
 }
 
