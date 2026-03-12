5aac05ac22bdedc69cbb1ee0a1ba203394ab0446
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-12 12:09:46 +0530

    Update home.js

--- SkyDrop-V1/js/home.js
+++ SkyDrop-V1/js/home.js
@@ -7,20 +7,22 @@ const container=document.getElementById("quizContainer");
 data.quizzes.forEach(q=>{
 
 const card=`
+
 <div class="col-md-4 mb-3">
 
-<div class="card p-3 shadow-sm"
+<div class="card shadow-sm p-3"
 style="cursor:pointer"
 onclick="openQuiz('${q.id}')">
 
 <div class="d-flex align-items-center">
 
 <img src="${q.icon}"
-style="width:40px;height:40px;margin-right:10px">
+width="40"
+class="me-3">
 
 <div>
 
-<h6 class="mb-1">${q.title}</h6>
+<h6>${q.title}</h6>
 <small>${q.questions.length} Questions</small>
 
 </div>
@@ -39,5 +41,7 @@ container.innerHTML+=card;
 });
 
 function openQuiz(id){
+
 window.location.href="quiz.html?quiz="+id;
+
 }
