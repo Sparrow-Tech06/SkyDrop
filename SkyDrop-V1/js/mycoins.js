70d7260ba2b532a7c265c675ac9f71c799d3ac30
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-12 06:38:28 +0530

    Update mycoins.js

--- SkyDrop-V1/js/mycoins.js
+++ SkyDrop-V1/js/mycoins.js
@@ -1,46 +1,51 @@
 let db;
 
-const request=indexedDB.open("QuizWallet",1);
+const request=indexedDB.open("QuizWallet",2);
 
 request.onupgradeneeded=function(e){
 
 db=e.target.result;
 
+if(!db.objectStoreNames.contains("wallet")){
 db.createObjectStore("wallet",{keyPath:"id"});
+}
+
+if(!db.objectStoreNames.contains("history")){
+db.createObjectStore("history",{autoIncrement:true});
+}
 
 };
 
 request.onsuccess=function(e){
-
 db=e.target.result;
-
 };
 
-/* callback function */
+function mycoin(gameName){
 
-function mycoin(){
+const tx=db.transaction(["wallet","history"],"readwrite");
 
-const tx=db.transaction("wallet","readwrite");
+const wallet=tx.objectStore("wallet");
+const history=tx.objectStore("history");
 
-const store=tx.objectStore("wallet");
-
-const get=store.get("user");
+const get=wallet.get("user");
 
 get.onsuccess=function(){
 
 let coins=0;
 
 if(get.result){
-
 coins=get.result.coins;
-
 }
 
 coins+=10;
 
-store.put({id:"user",coins:coins});
+wallet.put({id:"user",coins:coins});
 
-// alert("10 Coins Added! Total: "+coins);
+history.add({
+game:gameName,
+coins:10,
+time:new Date().toLocaleString()
+});
 
 };
 
