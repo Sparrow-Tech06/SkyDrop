adbed9c12cdda35cc0cd65a4610d13753d861203
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-16 12:34:14 +0530

    Create mycoins.js

---
+++ js/mycoins.js
@@ -0,0 +1,62 @@
+let db;
+
+const request = indexedDB.open("QuizWallet",1);
+
+request.onupgradeneeded=function(e){
+
+db=e.target.result;
+
+db.createObjectStore("wallet",{keyPath:"id"});
+db.createObjectStore("history",{autoIncrement:true});
+
+};
+
+request.onsuccess=function(e){
+db=e.target.result;
+};
+
+function mycoin(gameName){
+
+const tx=db.transaction(["wallet","history"],"readwrite");
+
+const wallet=tx.objectStore("wallet");
+const history=tx.objectStore("history");
+
+const get=wallet.get("user");
+
+get.onsuccess=function(){
+
+let coins=0;
+
+if(get.result){
+coins=get.result.coins;
+}
+
+coins+=10;
+
+wallet.put({id:"user",coins:coins});
+
+/* history save */
+
+history.add({
+game:gameName,
+coins:10,
+date:new Date().toLocaleString()
+});
+
+// alert("10 Coins Added");
+
+
+/* 👉 Android WebView notify */
+
+if (window.Android && typeof Android.onCoinAdded === "function") {
+
+Android.onCoinAdded();   // only notify
+
+// Android.onCoinAdded(10);  // notify + coin amount
+
+}
+
+};
+
+}
