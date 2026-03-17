b133e7dbf86bfb728da37c0b1f6a22db0bea296f
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-17 21:15:46 +0530

    Update mycoins.js

--- js/mycoins.js
+++ js/mycoins.js
@@ -2,22 +2,16 @@
 
 function mycoin(quizId, quizName){
 
-    let rewardKey = "reward_" + quizId;
-
-    // prevent duplicate reward
-    if(localStorage.getItem(rewardKey)){
-        console.log("Already rewarded for this quiz");
-        return;
-    }
-
+    // get existing coins
     let coins = parseInt(localStorage.getItem("coins")) || 0;
 
+    // add 10 coins every time
     coins += 10;
 
+    // save coins
     localStorage.setItem("coins", coins);
-    localStorage.setItem(rewardKey, true);
 
-    // history save
+    // save history
     let history = JSON.parse(localStorage.getItem("coinHistory")) || [];
 
     history.push({
@@ -28,5 +22,5 @@ function mycoin(quizId, quizName){
 
     localStorage.setItem("coinHistory", JSON.stringify(history));
 
-    console.log("10 coins added");
+    console.log("10 coins added for quiz:", quizName);
 }
